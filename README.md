f815b204cf2abc0fa59be345f295a634bc619374
6100m Detavex <106403202+5t4ryu@users.noreply.github.com>
2022-09-21 17:50:29 +0000

    Add memory patching middleware and add registry tweak skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Note: this repo contains code from Winclassic written by anixx. You can find that at https://winclassic.boards.net/thread/132/removing-navigation-bar-windows-explorer
+
+# This readme is a skeleton also and needs to be improved. TODO?
--- compiler.sh
+++ compiler.sh
@@ -1,1 +1,19 @@
 # Cygwin/mingw tasks go here
+
+# Make Build Directory
+mkdir build
+
+# Make build directory for ISO injections
+mkdir build\sources\
+
+
+# Make directory for registry
+# injections within the ISO injections
+# folder
+mkdir build\sources\$RegTweaks$
+
+
+# Copy reg files to the folder
+# for registry injections within
+# the ISO injections folder
+cp -r reg/* build\sources\$RegTweaks$\
