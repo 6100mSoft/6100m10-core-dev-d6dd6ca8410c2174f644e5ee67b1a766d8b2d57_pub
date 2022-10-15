7abc0b35f424491f59799e7d8d1acb2a89505edf
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:05:39 -0600

    Update CMSServiceLauncher.ps1

--- builtin/cms/CMSServiceLauncher.ps1
+++ builtin/cms/CMSServiceLauncher.ps1
@@ -1,7 +1,6 @@
 Write-Host "6100m10 CMS is starting...."
 Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
 Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactWebService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactScheduler.cmd" -ArgumentList $argstr -Wait -NoNewWindow
 Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
 Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLAgentYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
 Start-Process cmd.exe -ArgumentList "/c net start W3SVC" -RedirectStandardOutput 
