c045fa3c2c179208f4cd992a6525206b1d6d1dc0
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:36:59 -0600

    Update Register6100m10CMSService.cmd

--- builtin/cms/Register6100m10CMSService.cmd
+++ builtin/cms/Register6100m10CMSService.cmd
@@ -4,7 +4,6 @@ sc.exe create "6100m10 CMS" binPath= "C:\Gloom10\CMS\StartCMSService.cmd"
 if %errorlevel% == 2 echo Could not register service.
 if %errorlevel% == 0 echo Service registered successfully.
 echo Errorlevel: %errorlevel%
-pause
 cls
 goto exiter
 :exiter
