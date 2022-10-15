635094ce8cd85bbc17f79560b0e66e5267bafba3
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:33:13 -0600

    Update CMSServiceLauncher.ps1

--- builtin/cms/CMSServiceLauncher.ps1
+++ builtin/cms/CMSServiceLauncher.ps1
@@ -1,9 +1,8 @@
-$argstr = ""
 Write-Host "6100m10 CMS is starting...."
-Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactWebService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLAgentYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactService.cmd" -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\StartCoreImpactWebService.cmd"  -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLYuya.cmd" -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLAgentYuya.cmd" -Wait -NoNewWindow
 Start-Process cmd.exe -ArgumentList "/c net start W3SVC" -RedirectStandardOutput 
 Start-Process cmd.exe -ArgumentList "/c net start SQLBrowser" -RedirectStandardOutput 
 Start-Process cmd.exe -ArgumentList "/c net start SQLWriter" -RedirectStandardOutput 
