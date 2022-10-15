af7f779ec0fb311131ddc00399f71b2d8e3f4b6b
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:02:46 -0600

    Update StartCheckmarx.ps1

--- builtin/cms/StartCheckmarx.ps1
+++ builtin/cms/StartCheckmarx.ps1
@@ -1,8 +1,5 @@
-Start-Process cmd.exe -ArgumentList "/c net start W3SVC" -RedirectStandardOutput 
-Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLAgentYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process cmd.exe -ArgumentList "/c net start SQLBrowser" -RedirectStandardOutput 
-Start-Process cmd.exe -ArgumentList "/c net start SQLWriter" -RedirectStandardOutput 
+Write-Host "WARNING: 6100m10 CMS will be started"
+Start-Process -FilePath "C:\Gloom10\CMS\StartCMSService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
 Start-Process cmd.exe -ArgumentList "/c net start CxJobsManager" -RedirectStandardOutput 
 Start-Process cmd.exe -ArgumentList "/c net start CxScanEngine" -RedirectStandardOutput 
 Start-Process cmd.exe -ArgumentList "/c net start CxScansManager" -RedirectStandardOutput 
