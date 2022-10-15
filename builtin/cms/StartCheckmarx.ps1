39eb9550d904b7aeefe9f721eb12060bef229d07
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:08:51 -0600

    Update StartCheckmarx.ps1

--- builtin/cms/StartCheckmarx.ps1
+++ builtin/cms/StartCheckmarx.ps1
@@ -1,5 +1,7 @@
+$argstr = ""
 Write-Host "WARNING: 6100m10 CMS will be started"
 Start-Process -FilePath "C:\Gloom10\CMS\StartCMSService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
+Write-Host "6100m10 CMS has started."
 Start-Process cmd.exe -ArgumentList "/c net start CxJobsManager" -RedirectStandardOutput 
 Start-Process cmd.exe -ArgumentList "/c net start CxScanEngine" -RedirectStandardOutput 
 Start-Process cmd.exe -ArgumentList "/c net start CxScansManager" -RedirectStandardOutput 
