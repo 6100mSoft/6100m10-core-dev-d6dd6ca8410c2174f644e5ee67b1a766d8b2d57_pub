110af3f0f3b9535e37b461c8dbd5ace4dc4ad0e1
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:03:14 -0600

    Update StartSonarQube.ps1

--- builtin/cms/StartSonarQube.ps1
+++ builtin/cms/StartSonarQube.ps1
@@ -1,3 +1,3 @@
-Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\StartMSSQLAgentYuya.cmd" -ArgumentList $argstr -Wait -NoNewWindow
+Write-Host "WARNING: 6100m10 CMS will be started"
+Start-Process -FilePath "C:\Gloom10\CMS\StartCMSService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
 Start-Process cmd.exe -ArgumentList "/c net start Sonarqube" -RedirectStandardOutput 
