a526ea4c9f779954e52c67165cb5411b40bd0bda
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:33:33 -0600

    Update LogonPolicy.ps1

--- builtin/cms/LogonPolicy.ps1
+++ builtin/cms/LogonPolicy.ps1
@@ -1,3 +1,2 @@
-$argstr = ""
-Start-Process -FilePath "C:\Gloom10\CMS\Unregister6100m10CMSService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Register6100m10CMSService.cmd" -ArgumentList $argstr -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\Unregister6100m10CMSService.cmd" -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\Register6100m10CMSService.cmd" -Wait -NoNewWindow
