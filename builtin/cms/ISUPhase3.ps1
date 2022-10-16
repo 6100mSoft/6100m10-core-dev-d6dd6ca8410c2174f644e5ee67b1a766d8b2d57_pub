22a2084aa1588587cd6cecde06ba6b8a83b69ed4
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-16 12:48:20 -0700

    Overhaul

--- builtin/cms/ISUPhase3.ps1
+++ builtin/cms/ISUPhase3.ps1
@@ -1,53 +1,51 @@
 # Part of this code was written by a university.
 Start-Process -FilePath "C:\Gloom10\CMS\WriteLabPrinterMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+$ScriptPath = Split-Path $MyInvocation.InvocationName
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\ProgramData\ISU\Programs\AddLabPrinters.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteDeepFreezeMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteDeepFreezeMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\ProgramData\ISU\Programs\dfucf.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteCVMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteCVMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\ProgramData\ISU\Programs\ISUInsertIntoCV.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteLabMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteLabMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\UnRegisterISULabsService.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteRegisterMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteRegisterMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\Programs\RegisterISULabsService.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteBootupMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteBootupMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\Programs\StartISULabsService.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WritePowerShellMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteSPMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteSPMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\ProgramData\ISU\Programs\ISUsp.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteCHKMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteCHKMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\ProgramData\ISU\Programs\updchk.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteResMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\ProgramData\ISU\Programs\QRes.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteResMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
+Start-Process -FilePath "C:\Gloom10\CMS\SetMaxResolution.cmd" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\WriteCheckMessage.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\PCRename.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteRenamedMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteRenamedMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\Wait.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteClientMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteClientMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -File "C:\ProgramData\ISU\Programs\gswebclient.exe" -ArgumentList "/GTX https://thedarktower.labs.its.isu.edu:7773/"
 $printerFile = "C:\ProgramData\ISU\print_scripts\KI.txt"
 if ($printerFile -match "KI") {
@@ -69,39 +67,39 @@ else {
         }
     }
 }
-Start-Process -FilePath "C:\Gloom10\CMS\WriteSpoolerMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\InvokeRestartPrintSpooler.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Waiter.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteKillMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\WriteSpoolerMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\InvokeRestartPrintSpooler.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\Waiter.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
+& "$ScriptPath\WriteKillMessage.ps1"
+& "$ScriptPath\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\KillPrintSpooler.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WaitForTerm.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteXMLMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WaitForTerm.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WriteXMLMessage.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\ProgramData\ISU\Programs\ISUAddScheduledTaskFromXML.exe" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteAddMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WriteAddMessage.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\UpdateServiceAdder.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteUpdateMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WriteUpdateMessage.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\UpdateServiceMapper.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteTMPMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WriteTMPMessage.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\CallTMPScript.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteTimeMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WriteTimeMessage.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
 Start-Process -FilePath "C:\Gloom10\CMS\ISUSetTime.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\WriteTaskMessage.ps1" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\WriteTaskMessage.ps1"
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1" 
 Start-Process -FilePath "C:\Gloom10\CMS\PrinterTasks.cmd" -Wait -NoNewWindow
-Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+& "$ScriptPath\C:\Gloom10\CMS\Wait4Seconds.ps1"
