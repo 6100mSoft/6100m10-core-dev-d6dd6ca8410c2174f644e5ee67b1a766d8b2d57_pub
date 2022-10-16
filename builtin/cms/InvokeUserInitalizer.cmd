4329c3a3a20348be8f305b291783b438d28961c8
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 19:51:09 -0700

    Update InvokeUserInitalizer.cmd

--- builtin/cms/InvokeUserInitalizer.cmd
+++ builtin/cms/InvokeUserInitalizer.cmd
@@ -1,15 +1,44 @@
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\TaskkillExplorer.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\UserInitalizer.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeGenerateShortcuts.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\TaskkillSysprep.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartRetrobar.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartBasicThemer2.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartGadgets.ps1"
-Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartAltTabTerminator.ps1"
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\ISUPhase1.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteShortcutMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeGenerateShortcuts.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteSysprepMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\TaskkillSysprep.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteRetroMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartRetrobar.ps1"'
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteThemeMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartBasicThemer2.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteGadgetMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartGadgets.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteTabMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartAltTabTerminator.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WritePhaseMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\ISUPhase2.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteSequenceMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\ISUPhase3.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteCursorMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartCursorFX.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteShellMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStartOpenShell.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteSoundMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeStart3RVX.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteRestartMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
 Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\InvokeRestartExplorer.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteInitMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\UserInitalizer.ps1"
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\WriteUpMessage.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Powershell.exe -executionpolicy remotesigned -File "C:\Gloom10\CMS\TaskKillBanner.ps1" -Wait -NoNewWindow
