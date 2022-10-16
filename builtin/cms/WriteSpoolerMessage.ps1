579ba929548f96d4a29b4b6f92496d4df57420ee
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 21:29:27 -0700

    Update WriteSpoolerMessage.ps1

--- builtin/cms/WriteSpoolerMessage.ps1
+++ builtin/cms/WriteSpoolerMessage.ps1
@@ -1,3 +1,3 @@
 $message = "Initalizing print spooler so it's terminateable.... Please wait."
-Remove-Item "C:\ProgramData\ISU\update_message.txt"
-Add-Content "C:\ProgramData\ISU\update_message.txt" $message
+Remove-Item "C:\ProgramData\ISU\UpdateMessage.txt"
+Add-Content "C:\ProgramData\ISU\UpdateMessage.txt" $message
