a68712748eaee71fd084384b0cddc318b1ba6c41
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 21:25:45 -0700

    Update WriteLabPrinterMessage.ps1

--- builtin/cms/WriteLabPrinterMessage.ps1
+++ builtin/cms/WriteLabPrinterMessage.ps1
@@ -1,3 +1,3 @@
 $message = "Adding default printers.... Please wait."
-Remove-Item "C:\ProgramData\ISU\update_message.txt"
-Add-Content "C:\ProgramData\ISU\update_message.txt" $message
+Remove-Item "C:\ProgramData\ISU\UpdateMessage.txt"
+Add-Content "C:\ProgramData\ISU\UpdateMessage.txt" $message
