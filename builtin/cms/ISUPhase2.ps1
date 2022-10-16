17a0333a06a0f72b92ba5e94f91cbdb7a16f96b4
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 19:41:49 -0700

    Update ISUPhase2.ps1

--- builtin/cms/ISUPhase2.ps1
+++ builtin/cms/ISUPhase2.ps1
@@ -1,1 +1,3 @@
-Start-Process -FilePath "C:\ProgramData\ISU\print_scripts\labprintersetup.cmd" -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\WriteConnectionMessage.ps1" -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\Wait4Seconds.ps1" -Wait -NoNewWindow
+Start-Process -FilePath "C:\Gloom10\CMS\LabPrinterSetupTool.cmd" -Wait -NoNewWindow
