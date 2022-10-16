3ecf5646e484c79014587c7edc6d0551da11cf8f
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 20:14:33 -0700

    Create FirefoxShortcutGenerator.cmd

---
+++ builtin/cms/FirefoxShortcutGenerator.cmd
@@ -0,0 +1,11 @@
+Dim objShortcut, objShell
+Set objShell = WScript.CreateObject ("Wscript.Shell")
+Const strWorkDir = "%USERPROFILE%"
+Const strProgramTitle1 = "Start Firefox Instances"
+Const strProgram1 = "C:\Gloom10\CMS\FirefoxMenu.cmd"
+Set objShortcut = objShell.CreateShortcut (strLPath & "\" & strProgramTitle & ".lnk")
+objShortcut.TargetPath = strProgram
+objShortcut.WorkingDirectory = strWorkDir
+objShortcut.Description = strProgramTitle
+objShortcut.Save
+WScript.Quit
