32dc296dd696099af8c3220b6e7f084256397424
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-15 17:29:08 -0700

    Create ISUPhase1.ps1

---
+++ builtin/cms/ISUPhase1.ps1
@@ -0,0 +1,2 @@
+Start-Process -FilePath "C:\ProgramData\ISU\misc_scripts\SetUpChromeSoftlink.cmd" -Wait -NoNewWindow
+Start-Process -FilePath "C:\ProgramData\ISU\misc_scripts\cleanup.cmd" -Wait -NoNewWindow
